b0b155293f81fd7bf10060129230dc695e992ee7
IvanGB <clase@barrasa.dev>
2025-02-04 13:21:35 +0100

    feat: add idEspia

--- odoo/addons/igb_barrasa_system/models/campana.py
+++ odoo/addons/igb_barrasa_system/models/campana.py
@@ -1,10 +1,21 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields
+from odoo import models, fields, api
+import random
 
 class Campana(models.Model):
     _name = 'igb_barrasa_system.campana_espia'
     _description = 'Campaña de espia web'
 
+    id_espia = fields.Integer(string='ID Espia', readonly=True, copy=False, index=True, unique=True)
     nombre = fields.Char(string='Nombre', required=True)
     imagen = fields.Binary(string='Imagen')
     fecha_creacion = fields.Date(string='Fecha de Creación', default=fields.Date.today)
+
+    @api.model
+    def create(self, vals):
+        while True:
+            random_id = random.randint(100000, 999999)
+            if not self.search([('id_espia', '=', random_id)]):
+                break
+        vals['id_espia'] = random_id
+        return super(Campana, self).create(vals)
